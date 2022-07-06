69a6ffdd9eb413ad33850eaf8ec73894734e3df9
Luis Felipe de Azevedo Melo <felipeluis610@gmail.com>
2022-07-05 21:12:32 -0300

    Create SignInButton Component

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,25 @@
+import { FaGithub } from '../../../node_modules/react-icons/fa'
+import {FiX} from '../../../node_modules/react-icons/fi';
+
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+    const isUserLoggedIn = true;
+
+    return isUserLoggedIn ? 
+      (  <button 
+        type="button"
+        className={styles.signInButton}>
+            <FaGithub color='#04d361'/>
+            Felipe Dot
+            <FiX color="#737380" className={styles.closeIcon}/>
+        </button>): (
+              <button 
+              type="button"
+              className={styles.signInButton}>
+                  <FaGithub color='#eba417'/>
+                  Sign in with Github
+              </button>
+        )
+    
+}
